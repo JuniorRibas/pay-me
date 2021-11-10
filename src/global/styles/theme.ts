8610180c3dc67a54e08a5e07a509a74738f15460
JuniorRibas <ribeiropsp2011@gmail.com>
2021-11-10 09:14:37 -0300

    Colors

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,5 +1,11 @@
 export default {
   light: {
+    colors: {
+      primary: "#7A4FBB",
+      black: "#0D0D0D",
+      shape: "#E3E3EB",
+      background: "#F4F7F9",
+    },
     fonts: {
       regular: "Roboto_400Regular",
       medium: "Roboto_500Medium",
@@ -7,6 +13,12 @@ export default {
     },
   },
   dark: {
+    colors: {
+      primary: "#7A4FBB",
+      black: "#0D0D0D",
+      shape: "#E3E3EB",
+      background: "#F4F7F9",
+    },
     fonts: {
       regular: "Roboto_400Regular",
       medium: "Roboto_500Medium",
