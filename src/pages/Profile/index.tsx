57d82593c7e60b6b6ebe894c9b9e5cade4d6a778
JuniorRibeiro <ribeiropsp2011@gmail.com>
2021-11-10 23:06:58 -0300

    Aliamento dos icons na tabBar e tela de perfil

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+import { Container } from "./styles";
+
+function Profile() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Profile;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,12 +8,12 @@
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Entypo } from "@expo/vector-icons";
 
 import KeysPix from "../pages/KeysPix";
 import Cadastro from "../pages/Cadastro";
+import Profile from "../pages/Profile";
 
 const Tab = createBottomTabNavigator();
 
@@ -25,12 +25,14 @@ export const AppRouter = () => {
           headerShown: false,
           tabBarActiveTintColor: "#603EB0",
           tabBarInactiveTintColor: "#CCCC",
+          tabBarLabelPosition: "beside-icon",
         }}
       >
         <Tab.Screen
           name="Chaves"
           component={KeysPix}
           options={{
+            headerTitleAlign: "left",
             tabBarIcon: ({ color }) => {
               return <Entypo name="list" size={24} color={color} />;
             },
@@ -43,6 +45,16 @@ export const AppRouter = () => {
             title: "Nova Chave",
             tabBarIcon: ({ color }) => {
               return <Entypo name="squared-plus" size={24} color={color} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Perfil"
+          component={Profile}
+          options={{
+            title: "Perfil",
+            tabBarIcon: ({ color }) => {
+              return <Entypo name="user" size={24} color={color} />;
             },
           }}
         />
